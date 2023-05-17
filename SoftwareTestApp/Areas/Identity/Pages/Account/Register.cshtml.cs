e69b35d587b537a8f9cc88775d3c0e1d69ea6707
marco hansen <jivamarco@gmail.com>
2023-05-17 14:00:33 +0200

    security update

--- SoftwareTestApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SoftwareTestApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -113,6 +113,7 @@ namespace SoftwareTestApp.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                user.TwoFactorEnabled = true;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
