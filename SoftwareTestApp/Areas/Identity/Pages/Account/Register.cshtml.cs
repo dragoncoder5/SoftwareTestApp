3478f51d0fb995e3902c5c62481005893567d08c
marco hansen <jivamarco@gmail.com>
2023-05-24 12:09:03 +0200

    blazor tests

--- SoftwareTestApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SoftwareTestApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -136,6 +136,8 @@ namespace SoftwareTestApp.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
+                    await CreateUserRole(Input.Email, "Admin");
+
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
